5472b0c9bfffa3d7a751479f7cdb062a003029f7
tonakai-s <rdorneles64@gmail.com>
2025-05-12 21:15:09 -0300

    fix: Footer mobile

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[component]
 pub fn Footer() -> impl IntoView {
     view! {
-        <footer class="w-full bg-blue-black dark:bg-iced-white grid justify-center items-center mt-6 fixed bottom-0">
+        <footer class="w-full bg-blue-black dark:bg-iced-white grid justify-center items-center h-[50px] fixed bottom-0">
             <div>
                 <p class="block text-iced-white dark:text-blue-black py-3">"EOF"</p>
             </div>
